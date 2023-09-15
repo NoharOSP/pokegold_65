ca3df3871a617469523273827fd83d6639d3c2e1
NoharOSP <76562209+NoharOSP@users.noreply.github.com>
2023-09-15 23:56:57 +0200

    Update evos_attacks.asm

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -3309,8 +3309,8 @@ TyranitarEvosAttacks:
 
 LugiaEvosAttacks:
 	db 0 ; no more evolutions
-	db 1, AEROBLAST
-	db 11, SAFEGUARD
+	db 1, SAFEGUARD
+	db 11, AEROBLAST
 	db 22, GUST
 	db 33, RECOVER
 	db 44, HYDRO_PUMP
@@ -3323,8 +3323,8 @@ LugiaEvosAttacks:
 
 HoOhEvosAttacks:
 	db 0 ; no more evolutions
-	db 1, SACRED_FIRE
-	db 11, SAFEGUARD
+	db 1, SAFEGUARD
+	db 11, SACRED_FIRE
 	db 22, GUST
 	db 33, RECOVER
 	db 44, FIRE_BLAST
